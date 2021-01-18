42fc8336e50a8a438595647067efb78bc12ba835
solideagency <suus@solide.agency>
2021-01-18 16:09:34 +0100

    first week assignment

---
+++ docs/week1/js/app.js
@@ -0,0 +1,57 @@
+//no use of global variables, IIFE
+(function(){
+
+  //strict mode
+  "use strict"
+
+  //design pattern: module
+  var app = {
+    init: function(){
+      console.log("start app");
+      getData.init();
+    }
+  }
+
+  var getData = {
+    init: function(){
+      //this keyword
+      this.request();
+    },
+    url: function(){
+      //keep your code readable
+      //where to find API link and how to use it
+      //take time to think first about a problem find a solution by chosing an API
+      const api = 'http://api.github.com/';
+      const method = 'users/solideagency';
+
+      return api + method;
+    },
+    request: function(){
+      //promise, async
+      fetch(this.url())
+      .then(function (response){
+        return response.json();
+      })
+      .then(function(data){
+        renderData.template(data);
+      })
+      .catch(function (error){
+        console.log("Error " + error);
+      })
+      //async
+      console.log("I'm the last one in the row")
+
+    }
+  }
+
+  //render data with template literals
+  var renderData = {
+    template: function(data){
+      var html = document.querySelector('body');
+      html.insertAdjacentHTML("afterbegin", `<p>Login: ${data.login}</p>`)
+    }
+  }
+
+  app.init();
+
+})();
--- docs/week2/js/app.js
+++ docs/week2/js/app.js
@@ -1,7 +1,10 @@
-
+//no use of global variables, IIFE
 (function(){
+
+  //strict mode
   "use strict"
 
+  //design pattern: module
   var app = {
     init: function(){
       console.log("start app");
@@ -11,25 +14,41 @@
 
   var getData = {
     init: function(){
+      //this keyword
       this.request();
     },
     url: function(){
+      //keep your code readable
+      //where to find API link and how to use it
+      //take time to think first about a problem find a solution by chosing an API
       const api = 'http://api.github.com/';
       const method = 'users/solideagency';
 
       return api + method;
     },
     request: function(){
+      //promise, async
       fetch(this.url())
       .then(function (response){
         return response.json();
       })
       .then(function(data){
-        console.log(data)
+        renderData.template(data);
       })
       .catch(function (error){
         console.log("Error " + error);
       })
+      //async
+      console.log("I'm the last one in the row")
+
+    }
+  }
+
+  //render data with template literals
+  var renderData = {
+    template: function(data){
+      var html = document.querySelector('body');
+      html.insertAdjacentHTML("afterbegin", `<p>${data.login}</p>`)
     }
   }
 
